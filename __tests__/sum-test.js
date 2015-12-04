77c9248eb3c25b774b6d07a0f00205757de0afb2
Abylay Ospan <aospan@netup.ru>
2015-12-04 11:19:45 +0300

    one more dimple sum test
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -5,4 +5,9 @@ describe('sum', function() {
     var sum = require('../sum');
     expect(sum(1, 2)).toBe(3);
   });
+  it('adds -1 + 2 to equal 1', function() {
+    var sum = require('../sum');
+    expect(sum(-1, 2)).toBe(1);
+  });
+
 });

67a45beb2f56e204dfef8b8d08b9d0ecccc24d5a
Abylay Ospan <aospan@netup.ru>
2015-12-04 15:54:42 +0300

    min one more condition cover. now should be 100% coverage by tests
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- __tests__/min-test.js
+++ __tests__/min-test.js
@@ -5,4 +5,8 @@ describe('min', function() {
     var min = require('../min');
     expect(min(1, 2)).toBe(1);
   });
+  it('find minimum in 2 or 1. correct result: 1', function() {
+    var min = require('../min');
+    expect(min(2, 1)).toBe(1);
+  });
 });
--- sum.js
+++ sum.js
@@ -1,7 +1,4 @@
 function sum(value1, value2) {
   return value1 + value2;
 }
-function sumNotTested(value1, value2) {
-  return value1 + value2;
-}
 module.exports = sum;
